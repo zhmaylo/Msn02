a5b92cb01e8b210422ee85ed2194a9b47e31ef00
zhmaylo <dzhmaylo@gmail.com>
2021-09-28 19:29:07 +0300

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.use(express.json({ extended: true }))
 
 app.use('/api/hello', require('./routes/hello.routes'))
