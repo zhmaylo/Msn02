4cb19ddfd8d1a649d7b2b4ebe56e9c147060b5b8
zhmaylo <dzhmaylo@gmail.com>
2021-09-29 09:46:55 +0300

    update app.js

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.use('/api/hello', require('./backend/routes/hello.routes'))
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '/client/build')))
+  // app.use('/', express.static(path.join(__dirname, '/client/build')))
+
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 app.listen(port, () => console.log(`Server has been started ${port}...`))
