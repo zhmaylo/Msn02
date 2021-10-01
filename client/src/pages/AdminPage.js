fcaae1c3a10adbfcaf3bb760b1f5f67e3e3bf81f
zhmaylo <dzhmaylo@gmail.com>
2021-10-01 15:24:08 +0300

    20211001-1523 update AdminPage

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,5 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useHttp } from "../api/httpApi";
+import Table from 'react-bootstrap/Table'
+import Container from "react-bootstrap/Container";
 
 export const AdminPage = () => {
     const { request } = useHttp()
@@ -7,10 +9,8 @@ export const AdminPage = () => {
     const [fet, setFet] = useState("hello");
     // let fet = "";
     const getLink = useCallback(async () => {
-        let fet = await  request('/admin/userlist', 'GET', null, {});
-        console.log('🚀 ~ file: AdminPage.js ~ line 11 ~ getLink ~ fet', fet);
-         fet = await request('/api/hello', 'POST', null, {});
-         console.log('🚀 ~ file: AdminPage.js ~ line 13 ~ getLink ~ fet', fet);
+        let fet = await request('/admin/userlist', 'GET', null, {});
+        console.log('🚀 ~ file: AdminPage.js ~ line 13 ~ getLink ~ fet', fet);
         return fet;
     }, [request])
 
@@ -21,9 +21,38 @@ export const AdminPage = () => {
     }, [getLink])
 
     return (
-        <>
-            <span> Admin Page  </span>
-
-        </>
+        // <div clasName="col-md-4 coll-lg-3 col-xl-4 col-xxl-5">
+            <Container className="col-9">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
+        // </div>
     )
 }
